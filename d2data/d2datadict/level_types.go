2e123a5469a805731deb6ddf75572618b6f84862
Ziemas <ziemas@ziemas.se>
2019-11-23 03:52:27 +0100

    Fix bug in level types reader

--- d2data/d2datadict/level_types.go
+++ d2data/d2datadict/level_types.go
@@ -25,31 +25,32 @@ var LevelTypes []LevelTypeRecord
 func LoadLevelTypes(fileProvider d2interface.FileProvider) {
 	data := strings.Split(string(fileProvider.LoadFile(d2resource.LevelType)), "\r\n")[1:]
 	LevelTypes = make([]LevelTypeRecord, len(data))
-	for i, line := range data {
+	for i, j := 0, 0; i < len(data); i, j = i+1, j+1 {
 		idx := -1
 		inc := func() int {
 			idx++
 			return idx
 		}
-		if len(line) == 0 {
+		if len(data[i]) == 0 {
 			continue
 		}
-		parts := strings.Split(line, "\t")
+		parts := strings.Split(data[i], "\t")
 		if parts[0] == "Expansion" {
+			j--
 			continue
 		}
-		LevelTypes[i].Name = parts[inc()]
-		LevelTypes[i].Id = dh.StringToInt(parts[inc()])
+		LevelTypes[j].Name = parts[inc()]
+		LevelTypes[j].Id = dh.StringToInt(parts[inc()])
 		for fileIdx := range LevelTypes[i].Files {
-			LevelTypes[i].Files[fileIdx] = parts[inc()]
-			if LevelTypes[i].Files[fileIdx] == "0" {
-				LevelTypes[i].Files[fileIdx] = ""
+			LevelTypes[j].Files[fileIdx] = parts[inc()]
+			if LevelTypes[j].Files[fileIdx] == "0" {
+				LevelTypes[j].Files[fileIdx] = ""
 			}
 
 		}
-		LevelTypes[i].Beta = parts[inc()] != "1"
-		LevelTypes[i].Act = dh.StringToInt(parts[inc()])
-		LevelTypes[i].Expansion = parts[inc()] != "1"
+		LevelTypes[j].Beta = parts[inc()] != "1"
+		LevelTypes[j].Act = dh.StringToInt(parts[inc()])
+		LevelTypes[j].Expansion = parts[inc()] != "1"
 	}
 	log.Printf("Loaded %d LevelType records", len(LevelTypes))
 }
